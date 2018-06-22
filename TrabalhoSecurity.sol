db303a52c66d09dd9c7c359298c5ea1fa8aaffa7
Wesley Mayk <w_mayk007@hotmail.com>
2018-06-21 23:23:16 -0300

    Update TrabalhoSecurity.sol

--- TrabalhoSecurity.sol
+++ TrabalhoSecurity.sol
@@ -2,24 +2,34 @@ pragma solidity ^0.4.0;
 
 contract ContratoPrevidenciario {
     struct Participante{
-        string private nome;
-        uint private idade;
+        //acusando erros com o "private" nos atributos, por isso tirei
+        string nome;
+        uint idade;
         //possíveis statusParticipante -> Ativo, Benefício(recebendo), Cancelado
-        string private status;
+        string statusPrimario;
          }
         
     address public planoPrevidenciario;
     mapping (address => Participante) public participantesPlano;
     
-    function ContratoPrevidenciario {
+    function ContratoPrevidenciario (){
         //planoPrevidenciario é quem cria o ContratoPrevidenciario
         planoPrevidenciario = msg.sender;
     }
     
-    function MatriculaParticipante (adress participante) {
-        if msg.sender != planoPrevidenciario return;
-        
-        
+    function MatriculaParticipante (address participante, uint age, string name) {
+        if (msg.sender != planoPrevidenciario) return;
+        participantesPlano[participante].statusPrimario = "Ativo";
+        participantesPlano[participante].idade = age;
+        participantesPlano[participante].nome = name;
+    }
+    
+    function realizarContribuicao(address plano, uint amount) returns (bool contribuicaoRealizada) {
+        if (balances[msg.sender] < amount || participantesPlano[msg.sender].statusPrimario != "Ativo" ) return false;
+        balances[msg.sender] -= amount;
+        balances[plano] += amount;
+        Transfer(msg.sender, receiver, amount);
+        return true;
     }
 
         

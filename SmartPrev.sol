3d41036ac45ee5e97be1a9370eceda1d7e0b753b
Wesley Mayk <w_mayk007@hotmail.com>
2018-06-22 00:26:35 -0300

    Update SmartPrev.sol

--- SmartPrev.sol
+++ SmartPrev.sol
@@ -1,22 +1,33 @@
 pragma solidity ^0.4.0;
 
 contract ContratoPrevidenciario {
+    
+    
     struct Participante{
         //acusando erros com o "private" nos atributos, por isso tirei
         string nome;
         uint idade;
         //possíveis statusParticipante -> Ativo, Benefício(recebendo), Cancelado
         string statusPrimario;
+        //modo de armazenar correto (???)
+        mapping (address => uint256) saldoParticipante;
          }
         
     address public planoPrevidenciario;
     mapping (address => Participante) public participantesPlano;
+    mapping (address => uint256) public saldoPlano;
     
+    //permite procurar por tal transação no futuro utilizando estes parametros indexados como filtro
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+   
+   
+  //constructor 
     function ContratoPrevidenciario (){
         //planoPrevidenciario é quem cria o ContratoPrevidenciario
         planoPrevidenciario = msg.sender;
     }
     
+    //
     function MatriculaParticipante (address participante, uint age, string name) {
         if (msg.sender != planoPrevidenciario) return;
         participantesPlano[participante].statusPrimario = "Ativo";
@@ -24,24 +35,34 @@ contract ContratoPrevidenciario {
         participantesPlano[participante].nome = name;
     }
     
+    //função para comparar duas strings pelo hash, retorna true se forem diferentes
+    function stringsDiferentes (string a, string b) view returns (bool){
+       return keccak256(a) != keccak256(b);
+   }
+    
+    //função para realizar contribuição caso o participante tenha saldo e esteja ativo.
     function realizarContribuicao(address plano, uint amount) returns (bool contribuicaoRealizada) {
-        if (balances[msg.sender] < amount || participantesPlano[msg.sender].statusPrimario != "Ativo" ) return false;
-        balances[msg.sender] -= amount;
-        balances[plano] += amount;
-        Transfer(msg.sender, receiver, amount);
+        if (saldoPlano[msg.sender] < amount || stringsDiferentes(participantesPlano[msg.sender].statusPrimario, "Ativo" )) return false;
+        saldoPlano[msg.sender] -= amount;
+        saldoPlano[plano] += amount;
+        Transfer(msg.sender, plano, amount);
         return true;
     }
 
-        
-    function setName (string newName) {
-        name = newName;
+    //função teste de setar nome ( orientação a objetos feelings =( )
+    function setName (address participante,string newName) {
+        participantesPlano[participante].nome = newName;
     }
     
-    function getName () returns (string) {
-        return name;
+    //função teste de pegar nome ( orientação a objetos feelings =( )
+    function getName (address participante) returns (string) {
+        return participantesPlano[participante].nome;
     }
     
 }
+
+
+// to do
 
 contract PlanoPrevidenciario {
     

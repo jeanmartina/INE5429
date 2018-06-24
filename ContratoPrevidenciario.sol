d99933706276a2139709426bc879325fd7325513
Wesley Mayk <w_mayk007@hotmail.com>
2018-06-24 02:12:03 -0300

    Create ContratoPrevidenciario.sol

---
+++ ContratoPrevidenciario.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.4.11;
+
+contract ContratoPrevidenciario {
+    
+    address owner = msg.sender;
+    mapping (address => contribuicoesInvalidez) planoInvalidez;
+    mapping (address => contribuicoesEducacionais) planoEducacional;
+    mapping (address => contribuicoesNormais) planoNormal;
+    Participante participante;
+    string statusContrato;
+    
+    struct Participante{
+        uint cpf;
+        uint idadeEntrada;
+        uint idadeSaida;
+        string status;
+        address enderecoParticipante;
+    }
+    
+    struct contribuicoesInvalidez {
+        address participante;
+        uint256 valor;
+    }
+    
+    struct contribuicoesNormais {
+        address participante;
+        uint256 valor;
+    }
+    
+    struct contribuicoesEducacionais {
+        address participante;
+        uint256 valor;
+    }
+    
+     modifier onlyOwner(address account) {
+        require(msg.sender == account);
+        _;
+    }
+    
+    //permite procurar por tal transação no futuro utilizando estes parametros indexados como filtro
+     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    
+     //função para comparar duas strings pelo hash, retorna true se forem diferentes
+     function stringsDiferentes (string a, string b) view returns (bool){
+       return keccak256(a) != keccak256(b);
+   }
+    
+    //criação do contrato previdenciario - constructor 
+    
+     constructor  (uint _cpfParticipante, uint _idadeEntradaParticipante, 
+    uint _idadeSaidaParticipante) returns ( bool success) {
+        
+        participante.cpf = _cpfParticipante;
+        participante.idadeEntrada = _idadeSaidaParticipante;
+        participante.idadeSaida = _idadeEntradaParticipante;
+        participante.enderecoParticipante = _enderecoParticipante;
+        success = true;
+    }
+    
+}
